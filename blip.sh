f61487f5982b6fbe149b5d339acbd4c6f1af218b
Paul W. Rankin <hello@paulwrankin.com>
2022-11-06 20:31:39 +1000

    Some progress

--- blip.sh
+++ blip.sh
@@ -18,11 +18,14 @@
 
 program=$(basename "$0")
 version=0.1.0
-blip_file="${BLIP_FILE:-${HOME}/.blip/bookmarks.tsv}"
+blip_file="${BLIP_FILE:-${HOME}/.bookmarks/bookmarks.tsv}"
 
 fail() { echo "$1"; exit 1; }
-int_p() { expr "$1" : '[0-9][0-9]*$' >/dev/null; }
+int_p() { expr "$1" : '[0-9][0-9]*$' > /dev/null; }
+tbc() { echo "$1 not yet implemented"; }
 
+# usage()
+# returns: usage string
 usage() {
 	cat <<EOF
 $program v$version usage:
@@ -42,8 +45,6 @@ commands:
     add TAGs to bookmarks matching QUERY
   $program edit QUERY
     edit bookmarks matching QUERY in \$EDITOR
-  $program fetchtitles
-    fetch all bookmark titles from the web
   $program help
     print this help message
   $program version
@@ -66,27 +67,41 @@ init() {
 	if [ -f "$blip_file" ]; then
 		fail "$blip_file already exists"
 	else
-		mkdir -p $(dirname "$blip_file")
 		touch "$blip_file"
 	fi
+	return 0
 }
 
-tbc() { echo "$1 not yet implemented"; }
+# get_linenum()
+get_linenum() {
+	awk -v url="$url" '$1 == url { print NR }' < "$blip_file"
+}
 
+# add(url)
+# append $url too $blip_file
+# returns: 0
 add() {
-	tbc "add"
+	[ -n "$1" ] || fail "Missing argument"
+	url="$1"
+	exists=$(awk -v url="$url" '$1 == url' < "$blip_file")
+	if [ -n "$exists" ]; then
+		fail "Bookmark $url already exists"
+	else
+		printf "%s\n" "$url" >> "$blip_file"
+	fi
+	return 0
 }
 
+# tag(query, tag, [tag ...])
+# returns 0
 tag() {
 	tbc "tag"
 }
 
+# edit(query)
+# returns: 0
 edit() {
 	tbc "edit"
-}
-
-fetchtitles() {
-	tbc "fetchtitles"
 }
 
 # collect()
@@ -99,19 +114,19 @@ collect() {
 # list(print_fields, print_linenum, search_scope, query)
 # returns: printed list of matching bookmarks
 list() {
-	print_fields="$1"
-	[ -n "$2" ] && print_linenum='FNR,'
+	fields="$1"
+	[ "$2" -gt 0 ] && linenum='FNR,'
 	search_scope="$3"
 	query="$4"
 	urls=$(collect)
 	if $(int_p "$query"); then
-		urls=$(echo "$urls" | awk -F "\t" "FNR == $query { print $print_linenum $print_fields }")
+		urls=$(echo "$urls" | awk -F "\t" "FNR == $query { print $linenum $fields }")
 	elif [ -n "$query" ]; then
-		urls=$(echo "$urls" | awk -F "\t" "$search_scope ~ /$query/ { print $print_linenum $print_fields }")
+		urls=$(echo "$urls" | awk -F "\t" "$search_scope ~ /$query/ { print $linenum $fields }")
 	else
-		urls=$(echo "$urls" | awk -F "\t" "{ print $print_linenum $print_fields }")
+		urls=$(echo "$urls" | awk -F "\t" "{ print $linenum $fields }")
 	fi
-	[ -n "$urls" ] && echo "$urls" | column -ts $'\t'
+	[ -n "$urls" ] && echo "$urls" | column -t
 }
 
 # browse(search_scope, query)
@@ -128,11 +143,11 @@ browse() {
 	elif [ -n "$query" ]; then
 		urls=$(echo "$urls" | awk -F "\t" "$search_scope ~ /$query/ { print \$1 }")
 	fi
-	url_count=$(echo "$urls" | wc -l)
+	url_count=$(echo "$urls" | wc -l | xargs)
 	if [ "$url_count" -gt "$browse_limit" ]; then
 		cat <<-EOF
 			Attempted to open $url_count bookmarks at once but limit is $browse_limit
-			Try opening fewer bookmarks or set a higher $BLIP_BROWSE_LIMIT
+			Try opening fewer bookmarks or set a higher \$BLIP_BROWSE_LIMIT
 		EOF
 		exit 1
 	else
@@ -141,23 +156,36 @@ browse() {
 			"$BLIP_BROWSER" "$url"
 		done
 	fi
+	return 0
 }
 
+# main(arg, [arg ...])
+# returns: 0
 main() {
 	print_linenum=1
 	print_fields='$1'
 	search_scope='$0'
+	while getopts 'htcnv' option; do
+		case "$option" in
+			(h)		usage; return 0 ;;
+			(v)		echo "$program v$version"; return 0 ;;
+			(t)		include_tags=1 ;;
+			(c)		include_comment=1 ;;
+			(n)		print_linenum=0 ;;
+		esac
+	done
+	shift $(( OPTIND - 1 ))
 	case "$1" in
-		(init)				init ;;
+		(init)			init ;;
 		(ls|list|find)		shift; list "$print_fields" "$print_linenum" "$search_scope" "$@" ;;
-		(a|add)				shift; add "$@" ;;
+		(a|add)			shift; add "$@" ;;
 		(o|open|b|browse)	shift; browse "$search_scope" "$@" ;;
-		(t|tag)				shift; tag "$@" ;;
-		(e|edit)			shift; edit "$@" ;;
-		(fetchtitles)		shift; fetchtitles ;;
-		(-v|v|version)		echo "$program v$version" ;;
-		(-h|--help|help|*)	usage ;;
+		(t|tag)			shift; tag "$@" ;;
+		(e|edit)		shift; edit "$@" ;;
+		(v|version)		echo "$program v$version"; return 0 ;;
+		(--help|help|*)		usage; return 0 ;;
 	esac
+	return 0
 }
 
 main "$@"
